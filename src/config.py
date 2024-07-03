d2b4ee7664b8b89aa3b03adcae96eccdaa86f161
asanchezlc <94367215+asanchezlc@users.noreply.github.com>
2024-07-03 16:30:31 +0200

    last commit

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 
 def get_config():
     return {
-        'base_url': 'http://localhost/APIRestCEMED/controllers',
+        'base_url': 'http://localhost/miniproyectoCEMED_apirest/controllers',
         'db_download_path': 'C://Users//Usuario//Documents//DownloadsFromAPIRest'
     }
